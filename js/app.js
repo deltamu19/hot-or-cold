bf287dda588c4016cd06831b9bc928a2f618ba83
deltamu19 <daryll.martinez@gmail.com>
2016-06-28 22:02:50 -0700

    add random function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,35 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	var random = function(min, max) {
+		return Math.random() * (max - min) + min
+	}
+
+	var guessNum = random(1,101);
+
+	var guess = function() {
+		var userGuess = $('#userGuess').val(); 
+		var html = '<li>' + userGuess + '</li>'
+		if (userGuess == guessNum) {
+			return 
+			
+		}
+		else if (userGuess > guessNum) {
+			$('#guessList').append(html)
+			$('#feedback').html('Too high')
+		}
+		else if (userGuess < guessNum) {
+			$('#guessList').append(html)
+			$('#feedback').html('Too low')
+		}
+		else () {
+			$('#feedback').html('Too high')
+		}
+	}
+
 });
 
 
+//choose random number
+//user guesses a number
+//divide and conquer
